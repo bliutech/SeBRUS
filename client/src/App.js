effeeacb9a2b9b6fb3f4636a653b39ae404e0d5b
Cy <cy@rosenbol.org>
2023-07-07 18:22:10 -0400

    messing with properly setting up the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import logo from './assets/logo.svg';
 import './styles/App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        </Routes>
+        </BrowserRouter>
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +25,11 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
 }
+
 
 export default App;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,1 +1,23 @@
-// TODO
+import { Link } from "react-router-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+function HomePage() {
+    document.title = "GSET23BSG";
+
+    return (
+        <div>
+            <p>
+                Hello this is our project on Blockchain for the Social Good
+            </p>
+
+            <div className="home-image-container">
+                <img src="https://replicate.com/api/models/stability-ai/stable-diffusion/files/57f3a284-9cc1-4447-90c2-518971a3a8a5/out-0.png" />
+                <img src="https://replicate.com/api/models/stability-ai/stable-diffusion/files/1e326180-9e4d-425f-b903-5bd97a1770dd/out-0.png" />
+                <img src="https://replicate.com/api/models/stability-ai/stable-diffusion/files/928234b0-22b4-4938-8552-1af213b7294f/out-0.png" />
+            </div>
+        </div>
+        );
+    }
+    
+    export { HomePage };
