d45f0f306d6238dfafc0f8f75a2b3b5d6ef6dbfa
rebekah-wang <rebekah.wanq@gmail.com>
2023-07-10 16:07:11 -0400

    added elements to signup

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,31 +1,62 @@
 import { Link } from "react-router-dom";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+// import '../styles/index.css';
+import styles from "../styles/pages/SignInPage.module.css";
 
 function SignIn() {
-  document.title = "GSET23BSG";
+    document.title = "signin";
 
-  return (
-    <div>
-      <p>Sign In Below!</p>
-      <form action="/LoginForm.jsx" method="post">
-        <input
-          type="email"
-          required
-          name="email"
-          placeholder="Your email"
-        ></input>
-        <input
-          type="password"
-          required
-          name="password"
-          placeholder="Your password"
-        ></input>
-        <input type="submit" value="Submit"></input>
-      </form>
-      <p></p>
-    </div>
-  );
-}
-
+    return (
+        
+        <div className={styles.rounded} style={{
+                border: '1px solid white',
+                borderTopLeftRadius: '10px',
+                borderTopRightRadius: '10px',                
+                borderBottomLeftRadius: '10px',
+                borderBottomRightRadius: '10px'
+            }}>
+            <p>
+                <br></br>
+                <header>Sign In</header>
+                <br></br>
+                <br></br>
+                </p>
+                    <form action="/LoginForm.jsx" method="post">
+                        <text>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enter your username:&nbsp;
+                        </text>
+                        <input type="email" required name="email" placeholder="Your email"></input>
+                        <text>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        </text>
+                        <br></br>
+                        <br></br>
+                        <text>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enter your password:&nbsp;
+                        </text>
+                        <input type="password" required name="password" placeholder="Your password"></input>
+                        <text>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        </text>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <button className={styles.button}>
+                            <buttonText>Submit</buttonText>
+                        </button>
+                        <button>
+                            <buttonText>test</buttonText>
+                        </button>
+                        <br></br>
+                        <br></br>
+                    </form>
+                <p>
+            </p>
+        </div>
+        );
+    }
+    
 export { SignIn };
